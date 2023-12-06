5adef9e0bd2e51b384faefaaf1fe6545348698f4
Enrico Carrano <28276248+EnricoEng@users.noreply.github.com>
2023-12-06 11:48:32 -0300

    teste fetch

--- javascript.js
+++ javascript.js
@@ -1,36 +1,28 @@
-// Exemplo de como fazer uma requisição GET usando fetch
-fetch('https://api.exemplo.com/dados', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  }
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch((error) => {
-  console.error('Erro:', error);
-});
+function fetchAPIsEventos() {
+    // Substitua 'url-da-sua-api' pela URL da API de onde você deseja buscar os dados dos eventos
+    var url = 'https://register-event.onrender.com/events';
 
-// Exemplo de como fazer uma requisição POST usando fetch
-fetch('https://api.exemplo.com/dados', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({chave: 'valor'}),
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch((error) => {
-  console.error('Erro:', error);
-});
+    // Faça uma requisição HTTP GET para a URL da API
+    fetch(url)
+        .then(response => response.json())  // Converta a resposta em JSON
+        .then(data => {  // Manipule os dados recebidos
+            // 'data' é um objeto JavaScript que contém os dados recebidos da API
 
-function comprar() {
-  // Código para realizar a compra
-  alert('Compra realizada com sucesso!');
-}
-
-function cancelar() {
-  // Código para cancelar a compra
-  alert('Compra cancelada com sucesso!');
+            // Você pode agora usar esses dados para preencher a grade de eventos
+            // Por exemplo, você pode criar um novo elemento div para cada evento e adicionar esse div à grade
+            var gridContainer = document.getElementById('grid-container');
+            data.content.forEach(evento => {
+                var gridItem = document.createElement('div');
+                gridItem.className = 'grid-item';
+                gridItem.innerHTML = '<img src="' + evento.imagem + '" alt="' + evento.name + '">' +
+                                     '<p>' + evento.name + '</p>' +
+                                     '<p>' + evento.startTime + '</p>' +
+                                     '<p>Local: ' + evento.place.address + ', ' + evento.place.city + '</p>' +
+                                     '<p>Preço: ' + evento.priceTicket + '</p>' +
+                                     '<button type="button" onclick="comprar()">Comprar</button>' +
+                                     '<button type="button" onclick="cancelar()">Cancelar</button>';
+                gridContainer.appendChild(gridItem);
+            });
+        })
+        .catch(error => console.error('Erro:', error));  // Trate quaisquer erros que possam ocorrer
 }
